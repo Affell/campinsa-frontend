5750093939064d3815c2019c8c68e6e0e2665ad0
axell <axel.lenroue@gmail.com>
2023-10-14 19:37:33 +0200

    true green screen + 1080p

--- src/components/card/packAnim.js
+++ src/components/card/packAnim.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./packAnim.scss";
 import Card from "./card";
-import video from "../../assets/images/pack/anim.webm";
+import video from "../../assets/images/pack/anim.mp4";
 import green from "../../assets/images/pack/steve.webm";
 
 
@@ -15,6 +15,7 @@ function PackAnim({ id, firstname, lastname, surname, score, image, poste, pays 
   const [animPoste, setAnimPoste] = useState(false);
   const [showCard, setShowCard] = useState(false);
   const [animCard, setAnimCard] = useState(false);
+  const [miniCard, setMiniCard] = useState(false);
   const [greenScreen, setGreenScreen] = useState(false);
 
   useEffect(() => {
@@ -27,18 +28,19 @@ function PackAnim({ id, firstname, lastname, surname, score, image, poste, pays 
 
   useEffect(() => {
 
-    setTimeout(() => setShowCountry(true), 4600);
-    setTimeout(() => setAnimCountry(true), 4700);
-    setTimeout(() => setShowCountry(false), 5500);
+    setTimeout(() => setShowCountry(true), 6100);
+    // setTimeout(() => setAnimCountry(true), 6150);
+    setTimeout(() => setShowCountry(false), 7850);
 
-    setTimeout(() => setShowPoste(true), 5700);
-    setTimeout(() => setAnimPoste(true), 5800);
-    setTimeout(() => setShowPoste(false), 6600);
+    setTimeout(() => setShowPoste(true), 7900);
+    // setTimeout(() => setAnimPoste(true), 7900);
+    setTimeout(() => setShowPoste(false), 9300);
 
-    setTimeout(() => setShowCard(true), 7400);
-    setTimeout(() => setAnimCard(true), 7500);
+    setTimeout(() => setShowCard(true), 9300);
+    setTimeout(() => setAnimCard(true), 10000);
 
-    setTimeout(() => setGreenScreen(true), 10000);
+    setTimeout(() => setMiniCard(true), 14000);
+    setTimeout(() => setGreenScreen(true), 14000);
 
   }, []);
 
@@ -49,9 +51,9 @@ function PackAnim({ id, firstname, lastname, surname, score, image, poste, pays 
         <source src={video} type="video/mp4" />
       </video>
 
-      {showCountry && <img alt="country" className={"reveal-country" + (animCountry ? " active" : "")} src={pays} />}
-      {showPoste && <h1 className={"reveal-poste" + (animPoste ? " active" : "")}>{poste}</h1>}
-      <Card id={id} firstname={firstname} lastname={lastname} surname={surname} score={score} image={image} poste={poste} pays={pays} show={showCard} active={animCard} />
+      {showCountry && <img alt="country" className="reveal-country" src={pays} />}
+      {showPoste && <h1 className="reveal-poste">{poste}</h1>}
+      <Card className={(miniCard ? "mini" : "")} id={id} firstname={firstname} lastname={lastname} surname={surname} score={score} image={image} poste={poste} pays={pays} show={showCard} active={animCard} />
       {greenScreen && <video className="anim-video" src={green} muted autoPlay />}
     </div>
   </>;

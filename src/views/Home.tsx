67c3d8135de574c0b545d9033db6445cab0ae5a3
Rubaine <vieira.ruben.sp@gmail.com>
2024-03-12 21:29:36 +0100

    inchala ça marche

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,44 +1,19 @@
-import NavBar from '../components/nav/Navbar';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './Home.css'; 
-import LinkCard from '../components/LinkCard/LinkCard';
+import NavBar from "../components/nav/Navbar";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
-        <NavBar/>
+      <NavBar />
       <div className="first-container">
         <div className="text-on-image principal">
           Bienvenue a Camp'INSA
-        <div className='text-on-image secondary'>
-          En savoir plus
-        </div>
+          <div className="text-on-image secondary">En savoir plus</div>
         </div>
       </div>
-      <div className='second-container'>
-        <Container>
-          <Row>
-            <Col>
-              <LinkCard imagePath='src\assets\images\homePage.jpg' to="/programme"/>
-            </Col>
-            <Col>
-              <LinkCard imagePath='src\assets\images\homePage.jpg' to="/programme"/>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <LinkCard imagePath='src\assets\images\homePage.jpg' to="/programme"/>
-            </Col>
-            <Col>
-              <LinkCard imagePath='src\assets\images\homePage.jpg' to="/programme"/>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <div className="second-container"></div>
     </div>
   );
-}
+};
 
 export default Home;

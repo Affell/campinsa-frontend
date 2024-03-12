d5f6835d94bd6cdb2c1a090e5f5dee39c28ca082
axell <axel.lenroue@gmail.com>
2024-03-12 23:22:47 +0100

    navbar

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -3,27 +3,29 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import { Link } from "react-router-dom"
+import NavBarToogler from "./NavBarToggler"
+import { useCycle } from "framer-motion"
 
-const NavBar = () => {
-    return (
-        <Navbar expand="lg" className="navbar bg-body-tertiary fixed-top">
-            <Container>
-                <Link className="navbar-brand" to="/">
-                    <img src="src\assets\images\logo.png" width="70" height="70" className="d-inline-block align-top" alt="Logo Camp'INSA" />
-                </Link>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Link className="nav-link" to="/">Accueil</Link>
-                        <Link className="nav-link" to="/planning">Programme</Link>
-                        <Link className="nav-link" to="/shotgun">Shotgun</Link>
-                        <Link className="nav-link" to="/teams">Teams</Link>
-                        <Link className="nav-link" to="/caritaxi">Cari Taxi</Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+export default function NavBar() {
+
+    const [isOpen, toggleOpen] = useCycle(false, true);
+
+    return <Navbar expanded={isOpen} expand="lg" className="navbar bg-body-tertiary fixed-top">
+        <Container>
+            <Link className="navbar-brand" to="/">
+                <img src="src\assets\images\logo.png" width="70" height="70" className="d-inline-block align-top" alt="Logo Camp'INSA" />
+            </Link>
+            <NavBarToogler toggle={toggleOpen} isOpen={isOpen} />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Link className="nav-link" to="/">Accueil</Link>
+                    <Link className="nav-link" to="/planning">Programme</Link>
+                    <Link className="nav-link" to="/shotgun">Shotgun</Link>
+                    <Link className="nav-link" to="/teams">Teams</Link>
+                    <Link className="nav-link" to="/caritaxi">CariTaxi</Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+
 }
-
-export default NavBar

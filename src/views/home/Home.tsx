a0d354a0839207c969c132a6edbfcf6a3250e414
Rubaine <vieira.ruben.sp@gmail.com>
2024-03-12 21:58:28 +0100

    home

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,17 +2,17 @@ import NavBar from "../../components/nav/Navbar";
 import "./Home.css";
 
 const Home = () => {
-  return <>
-    <NavBar />
-    <div className="home-container">
-      <div className="first-container">
+  return (
+    <>
+      <NavBar />
+      <div className="first-container fullscreen-container">
         <div className="text-on-image principal">
           Bienvenue a Camp'INSA
           <div className="text-on-image secondary">En savoir plus</div>
         </div>
       </div>
-    </div>
-  </>
+    </>
+  );
 };
 
 export default Home;

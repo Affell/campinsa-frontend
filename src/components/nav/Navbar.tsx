2b0a6df6c3abd2ddc27fabc967053b8785b1d842
RVieira <vieira.ruben.sp@gmail.com>
2024-03-12 09:42:25 +0100

    React-Bootstrap le poulet

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,25 @@
+import "./Navbar.css"
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+
+const NavBar = () => {
+  return (
+    <Navbar expand="lg" className="navbar bg-body-tertiary">
+        <Container>
+            <Navbar.Brand href="#home">
+                <img src="src\assets\images\logo.png" width="70" height="70" className="d-inline-block align-top" alt="Logo Camp'INSA"/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Accueil</Nav.Link>
+                    <Nav.Link href="#programme">Programme</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar

c434b42bdd08ea3d188b2452c9b0adc94c7a7513
axell <axel.lenroue@gmail.com>
2024-03-12 23:44:12 +0100

    fix navbar width

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,26 +1,22 @@
-import AliceCarousel from "react-alice-carousel";
 import NavBar from "../../components/nav/Navbar";
 import "./Home.css";
 import Carousel from "../../components/Carousel/Carousel";
 
-const Home = () => {
-  return (
-    <>
-      <NavBar />
-      <div className="first-container fullscreen-container">
-        <div className="text-on-image principal">
-          Bienvenue a Camp'INSA
-          <div className="text-on-image secondary">En savoir plus</div>
-        </div>
+export default function Home() {
+  return <>
+    <NavBar />
+    <div className="first-container fullscreen-container">
+      <div className="text-on-image principal">
+        Bienvenue a Camp'INSA
+        <div className="text-on-image secondary">En savoir plus</div>
       </div>
-      <div className="second-container fullscreen-container">
-        <div className="sponsors">Nos partenaires</div>
-        <div className="carousel">
-          <Carousel />
-        </div>
+    </div>
+    <div className="second-container fullscreen-container">
+      <div className="sponsors">Nos partenaires</div>
+      <div className="carousel">
+        <Carousel />
       </div>
-    </>
-  );
+    </div>
+  </>
+    ;
 };
-
-export default Home;

0644d4fea2ec3063a555af5886fb5d9697336ea7
RVieira <vieira.ruben.sp@gmail.com>
2024-03-18 11:19:10 +0100

    team page

---
+++ src/components/TeamPage/TeamPage.tsx
@@ -0,0 +1,14 @@
+interface Props{
+    teamName: string;
+    teamDetail: string;
+    members: 
+}
+const TeamPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TeamPage

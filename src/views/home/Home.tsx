8f606d880724e21b1adb12a89696c0b803079090
axell <axel.lenroue@gmail.com>
2024-03-20 21:34:28 +0100

    ajust home

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,20 +3,27 @@ import "./Home.css";
 import Carousel from "../../components/Carousel/Carousel";
 import Footer from "../../components/nav/Footer";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
+import arrowDown from "../../assets/images/arrow.png";
 
 export default function Home() {
+
+  scrollBy()
   return (
     <>
       <NavBar />
       <div className="first-container fullscreen-container">
         <div className="text-on-image principal">
           Bienvenue à Camp'INSA
-          <div className="text-on-image secondary">En savoir plus</div>
+          <div className="text-on-image secondary" onClick={() => scrollBy(0, innerHeight - 80 - scrollY)}>
+            En savoir plus
+            <img src={arrowDown} className="arrow-down" />
+          </div>
+
         </div>
       </div>
-      <div className="video-container text-center">
+      <div id="video" className="video-container text-center">
         <p>Retrouvez notre film ici !</p>
-        <hr className="w-50 mx-auto mb-5 mb-xl-9 border-dark-subtle"></hr>
+        <hr className="w-50 mx-auto mb-1 border-dark-subtle"></hr>
         <div className="video-player">
           <VideoPlayer videoId="cjaQCkNlYDg" />
         </div>

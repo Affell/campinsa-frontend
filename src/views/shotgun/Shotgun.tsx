c7f877b8daaed6b52ba0e38b8537e69927a30742
Rubaine <vieira.ruben.sp@gmail.com>
2024-04-10 19:07:38 +0200

    shotguns

--- src/views/shotgun/Shotgun.tsx
+++ src/views/shotgun/Shotgun.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import NavBar from "../../components/nav/Navbar";
 import "./Shotgun.css";
 import { useEffect, useState } from "react";
@@ -6,7 +6,6 @@ import { getFetch } from "../../core/api/fetch";
 import { TimeLeft, calculateTimeLeft } from "../../core/utils/date";
 import { Config } from "../../core/config/global";
 import Footer from "../../components/nav/Footer";
-import rapJeu from "../../assets/images/Rap-jeu.png";
 
 type Shotgun = {
   id: number;
@@ -33,43 +32,47 @@ const Timer = ({ date, id }: { date: Date; id: number }) => {
   }, [date]);
 
   return (
-    <>
+    <div className="timer-container">
       {remainingTime(timeLeft) === "0d 0h 0m 0s" ? (
-        <Button
+        <a
           href={Config.Urls.API + "/shotgun/link/" + id}
-          target="_blank"
-        />
+          className="shotgun-button"
+        >
+          <button type="button" className="btn btn-primary btn-sm">
+            Shotgun
+          </button>
+        </a>
       ) : (
-        remainingTime(timeLeft)
+        <p>{remainingTime(timeLeft)}</p> // Encapsulez le texte dans une balise <p>
       )}
-    </>
+    </div>
   );
 };
 
-const dailyShotguns: Shotgun[] = [
-  {
-    id: 1,
-    unlockTime: new Date("2024-04-10T01:26:00+02:00"),
-    imageBytes: "",
-    name: "Rap Jeu",
-  },
-  {
-    id: 2,
-    unlockTime: new Date("2024-04-15T21:30:00Z"),
-    imageBytes: "",
-    name: "Diverty Box",
-  },
-  {
-    id: 3,
-    unlockTime: new Date("2024-04-15T21:30:00Z"),
-    imageBytes: "",
-    name: "Crepes",
-  },
-];
+// const dailyShotguns: Shotgun[] = [
+//   {
+//     id: 1,
+//     unlockTime: new Date("2024-04-10T18:19:00+02:00"),
+//     imageBytes: "",
+//     name: "Aprem Sportive",
+//   },
+//   {
+//     id: 2,
+//     unlockTime: new Date("2024-04-10T18:03:00+02:00"),
+//     imageBytes: "",
+//     name: "Diverty Box",
+//   },
+//   {
+//     id: 3,
+//     unlockTime: new Date("2024-04-15T21:30:00+02:00"),
+//     imageBytes: "",
+//     name: "Crepes",
+//   },
+// ];
 
 export default function Events() {
   const [loading, setLoading] = useState(false);
-  // const [dailyShotguns, setDailyShotguns] = useState<Shotgun[]>([]);
+  const [dailyShotguns, setDailyShotguns] = useState<Shotgun[]>([]);
   const [thursday, setThursday] = useState<Shotgun>();
   const [friday, setFriday] = useState<Shotgun>();
 
@@ -77,18 +80,18 @@ export default function Events() {
     getFetch(
       "/shotgun/list",
       (data) => {
-        // if (data?.shotguns) {
-        //   let temp: Shotgun[] = [];
-        //   for (const s of data.shotguns) {
-        //     temp.push({
-        //       id: s.id,
-        //       unlockTime: new Date(s.unlock_time * 1000),
-        //       imageBytes: s.image_bytes,
-        //       name: s.name,
-        //     });
-        //   }
-        //   setDailyShotguns(temp);
-        // }
+        if (data?.shotguns) {
+          let temp: Shotgun[] = [];
+          for (const s of data.shotguns) {
+            temp.push({
+              id: s.id,
+              unlockTime: new Date(s.unlock_time * 1000),
+              imageBytes: s.image_bytes,
+              name: s.name,
+            });
+          }
+          setDailyShotguns(temp);
+        }
 
         if (data?.thursday) {
           setThursday({
@@ -133,7 +136,6 @@ export default function Events() {
                   <div className="text-overlay bottom-right">
                     <p className="name">{thursday.name}</p>
                     <p className="shotgun">
-                      Shotgun Disponible dans: <br />
                       <Timer date={thursday.unlockTime} id={thursday.id} />
                     </p>
                   </div>
@@ -153,7 +155,6 @@ export default function Events() {
                   <div className="text-overlay bottom-left">
                     <p className="name">{friday.name}</p>
                     <p className="shotgun">
-                      Shotgun Disponible dans: <br />
                       <Timer date={friday.unlockTime} id={friday.id} />
                     </p>
                   </div>
@@ -168,17 +169,23 @@ export default function Events() {
         Mobile version (responsive) */}
         <div className="daily-event">
           <p className="today">Aujourd'hui</p>
-          <Row xs={1} md={3} className="g-4">
+          <Row
+            xs={1}
+            md={3}
+            className={`g-4 ${
+              dailyShotguns.length === 1 ? "justify-content-md-center" : ""
+            }`}
+          >
             {dailyShotguns.map((event, index) => (
               <Col key={index} className="event">
                 <div className="event-card">
-                  <Image src={rapJeu} className="img-fluid image-daily-event" />
+                  <Image
+                    src={`data:image/png;base64,${event.imageBytes}`}
+                    className="img-fluid image-daily-event"
+                  />
                   <div className="event-info">
                     <p className="name">{event.name}</p>
-                    <p className="shotgun">
-                      Shotgun disponible dans: <br />
-                      <Timer date={event.unlockTime} id={event.id} />
-                    </p>
+                    <Timer date={event.unlockTime} id={event.id} />
                   </div>
                 </div>
               </Col>

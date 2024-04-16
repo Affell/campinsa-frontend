ff7b541c677d2e635b3ce02155465521c6f67dfe
RVieira <vieira.ruben.sp@gmail.com>
2024-04-16 17:11:27 +0200

    bob

---
+++ src/views/bob/Bob.tsx
@@ -0,0 +1,12 @@
+import "./Bob.css"
+
+export default function Bob(){
+    return(
+        <>
+        <div className="bob">
+            <h1>T'as trouvé mom bob</h1>
+            <h2>Viens voir le mec avec un pull paquerette et dit: <br/>"Ruben foudroie Axel Lenroue <del>en cyber</del>"</h2>
+        </div>
+        </>
+    )
+}
